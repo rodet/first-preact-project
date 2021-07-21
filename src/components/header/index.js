e67b4efc744fd354a5557f190272a530b8406942
Stephane Rodet <RODET@de.ibm.com>
2021-07-21 09:37:53 +0200

    feat: include the UI shell, improve SCSS loading
    Important variables were missing from the SCSS

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,68 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
-import style from './style.css';
+//import style from './style.css';
 
-const Header = () => (
-	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
-	</header>
+import {
+	Header,
+	HeaderContainer,
+	HeaderName,
+	HeaderNavigation,
+	HeaderMenuButton,
+	HeaderMenuItem,
+	HeaderGlobalBar,
+	HeaderGlobalAction,
+	SkipToContent,
+	SideNav,
+	SideNavItems,
+	HeaderSideNavItems,
+} from 'carbon-components-react';
+
+const TutorialHeader = () => (
+	<HeaderContainer
+		render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+			<Header aria-label="Carbon Tutorial">
+				<SkipToContent />
+				<HeaderMenuButton
+					aria-label="Open menu"
+					onClick={onClickSideNavExpand}
+					isActive={isSideNavExpanded}
+				/>
+				<HeaderName href="/" prefix="IBM">
+					Carbon Tutorial
+				</HeaderName>
+				<HeaderNavigation aria-label="Carbon Tutorial">
+					<HeaderMenuItem href="/profile">Me</HeaderMenuItem>
+					<HeaderMenuItem href="/profile/john">John</HeaderMenuItem>
+				</HeaderNavigation>
+				<SideNav
+					aria-label="Side navigation"
+					expanded={isSideNavExpanded}
+					isPersistent={false}>
+					<SideNavItems>
+						<HeaderSideNavItems>
+							<HeaderMenuItem href="/profile">Me</HeaderMenuItem>
+							<HeaderMenuItem href="/profile/john">John</HeaderMenuItem>
+						</HeaderSideNavItems>
+					</SideNavItems>
+				</SideNav>
+				<HeaderGlobalBar />
+			</Header>
+		)}
+	/>
 );
 
-export default Header;
+export default TutorialHeader;
+
+
+// const Header = () => (
+// 	<header class={style.header}>
+// 		<h1>Preact App</h1>
+// 		<nav>
+// 			<Link activeClassName={style.active} href="/">Home</Link>
+// 			<Link activeClassName={style.active} href="/profile">Me</Link>
+// 			<Link activeClassName={style.active} href="/profile/john">John</Link>
+// 		</nav>
+// 	</header>
+// );
+
+// export default Header;
